3b2a1642f74f579612f978aadaa4ed06e45ebef8
Raghu <Raghu@193.11.187.78>
2019-03-18 19:16:03 +0100

    commited

---
+++ Maven9/src/main/java/Group9/Maven9/App.java
@@ -0,0 +1,13 @@
+package Group9.Maven9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Maven9/src/test/java/Group9/Maven9/CalculatorTest.java
@@ -0,0 +1,33 @@
+package Group9.Maven9;
+
+import static org.hamcrest.CoreMatchers.is;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	@Test
+    public void addTwoThree() throws Exception {
+        final int result = new Calculator().add(2, 3);
+        assertThat(result, is(5));
+    }
+    
+    @Test
+    public void subThreeThree() throws Exception {
+        final int result = new Calculator().subtract(3, 3);
+        assertThat(result, is(0));
+    }
+    
+    @Test
+    public void mulThreeThree() throws Exception {
+        final int result = new Calculator().multiply(3, 3);
+        assertThat(result, is(9));
+    }
+    
+    @Test
+    public void divThreeMinusThree() throws Exception {
+        final Long result = (long) new Calculator().divide(3, -3);
+        assertThat(result, is(-1L));
+    }
+}
